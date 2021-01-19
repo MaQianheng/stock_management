0ad2e040186cf5cb3c6346ef2eb54743dda8666a
MaQianheng <894585638@qq.com>
2021-01-19 17:58:57 +0800

    adding dist subtree

---
+++ stock_management/precache-manifest.2abcbdbd8af441d8a667c074a5724a74.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd8d90e939e63d30930394f9061747b",
+    "url": "/stock_management/css/_custom.css"
+  },
+  {
+    "revision": "799d34d596f37a6b1b17",
+    "url": "/stock_management/css/app.9268010d.css"
+  },
+  {
+    "revision": "1e4c138641f8577dafd7",
+    "url": "/stock_management/css/chunk-vendors.f33fdbe6.css"
+  },
+  {
+    "revision": "dede9f6429f6f447a4c4",
+    "url": "/stock_management/css/demo.7d2d9aa4.css"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/stock_management/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/stock_management/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/stock_management/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/stock_management/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/stock_management/img/brand/favicon.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/stock_management/img/brand/green.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/stock_management/img/brand/white.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/stock_management/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/stock_management/img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/stock_management/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/stock_management/img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/stock_management/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/stock_management/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "07bcd6092912294d4b9847c85e10260a",
+    "url": "/stock_management/img/theme/supplier-diversity.jpg"
+  },
+  {
+    "revision": "dc79f87290b2b4bb1455963246eecdb3",
+    "url": "/stock_management/img/theme/supplier.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/stock_management/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/stock_management/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/stock_management/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/stock_management/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "498d600c434a3808564afa34f09b3d93",
+    "url": "/stock_management/img/theme/user-icon-hd.png"
+  },
+  {
+    "revision": "60d7f6e38534501a8bc1f5576bc1fa3b",
+    "url": "/stock_management/img/theme/user-icon.jpg"
+  },
+  {
+    "revision": "a45f7ffa9a0f584f76b5ac219d9703ec",
+    "url": "/stock_management/img/theme/user_1.png"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/stock_management/img/theme/vue.jpg"
+  },
+  {
+    "revision": "f02886757083c60e13926aacf409e92c",
+    "url": "/stock_management/index.html"
+  },
+  {
+    "revision": "799d34d596f37a6b1b17",
+    "url": "/stock_management/js/app.b563ef3a.js"
+  },
+  {
+    "revision": "1e4c138641f8577dafd7",
+    "url": "/stock_management/js/chunk-vendors.def4886a.js"
+  },
+  {
+    "revision": "dede9f6429f6f447a4c4",
+    "url": "/stock_management/js/demo.90bc3292.js"
+  },
+  {
+    "revision": "2c6190eb814d2dd4d454ae170c43c7c2",
+    "url": "/stock_management/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/stock_management/robots.txt"
+  }
+]);
